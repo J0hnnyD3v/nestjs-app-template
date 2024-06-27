87f13434810f7360be487c1a335bc7a83f2f7213
Jonathan <coding.with.johnny.dev@gmail.com>
2024-06-27 12:33:59 -0600

    core: add first swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
-import { AppModule } from './app.module';
-import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+
+  const config = new DocumentBuilder()
+    .setTitle('Nest JS Template')
+    .setDescription('The Nest JS API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 
